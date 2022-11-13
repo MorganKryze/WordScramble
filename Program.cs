ce51fea5c45d725824f73f3b6a41176d3e5f66ee
Brioche <thomasderudder@sfr.fr>
2022-11-13 18:09:33 +0100

    transform txt file in dictonary and json

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.IO;
 using static System.Console;
+using Newtonsoft.Json;
 namespace World_Scramble
 {
     class MainProgram
     {
+        //create a function that return  Dictionary<string,string[]> and take path string in argument
+        public static Dictionary<string, string[]> ReadTxtToJSON(string path)
+        {
+            //read the json file and store it in a string
+            string json = File.ReadAllText(path);
+            //create arrays of odd elements in lines (keys)
+            string[] Key = lines.Where((x, i) => i % 2 == 0).ToArray();
+            //create arrays of even elements in lines (values)
+            string[][] Value = lines.Where((x, i) => i % 2 != 0).ToArray().Select(x => x.Split(' ')).ToArray();
+            //dictionnary creation
+            Dictionary<string,string[]> data = Key.Zip(Value, (s, i) => new { s, i }).ToDictionary(item => item.s, item => item.i);
+
+            string json=JsonConvert.SerializeObject( data );
+            return data;
+        }
+
         static void Main(string[] args)
         {
-            Write("hello world");
+            Dictionary<string, string[]>dict=ReadTxtToJSON("MotsPossiblesFR.txt");
         }
     }
 }
