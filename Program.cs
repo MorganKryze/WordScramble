fea1b49af79aefccb65f397f961b9a92cb1e0a44
Brioche <thomasderudder@sfr.fr>
2022-11-16 12:38:45 +0100

    Bug correction

--- Program.cs
+++ Program.cs
@@ -1,30 +1,14 @@
-using System;
-using System.IO;
+using System.Diagnostics;
 using static System.Console;
 using Newtonsoft.Json;
-namespace World_Scramble
+namespace Word_Scramble
 {
     class MainProgram
     {
-        //create a function that return  Dictionary<string,string[]> and take path string in argument
-        public static Dictionary<string, string[]> ReadTxtToJSON(string path)
-        {
-            //read the json file and store it in a string
-            string[] lines = File.ReadAllLines(path);
-            //create arrays of odd elements in lines (keys)
-            string[] Key = lines.Where((x, i) => i % 2 == 0).ToArray();
-            //create arrays of even elements in lines (values)
-            string[][] Value = lines.Where((x, i) => i % 2 != 0).ToArray().Select(x => x.Split(' ')).ToArray();
-            //dictionnary creation
-            Dictionary<string,string[]> data = Key.Zip(Value, (s, i) => new { s, i }).ToDictionary(item => item.s, item => item.i);
 
-            string json=JsonConvert.SerializeObject( data );
-            return data;
-        }
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Dictionary<string, string[]>dict=ReadTxtToJSON("MotsPossiblesFR.txt");
+            Dictionary<string, string[]> dict = DictionaryMethod.ReadTxtToJSON("dataDictionary/MotsPossiblesFR.txt", "data.json");
         }
     }
 }
