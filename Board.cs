45dacc279ad04423e4c7e384882c242790046eb1
Brioche <thomasderudder@sfr.fr>
2022-12-02 17:34:36 +0100

    Get Lines and dictionnary

--- Board.cs
+++ Board.cs
@@ -2,11 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Word_Scramble
 {
     public class Board
     {
-        
+        public static char[] getRow(int row, char[,] board)
+        {
+            return( Enumerable.Range(0, board.GetLength(1)).Select(y => board[row, y]).ToArray());
+        }
+        public static char[] getColumn(int column, char[,] board)
+        {
+            return( Enumerable.Range(0, board.GetLength(1)).Select(x => board[x, column]).ToArray());
+        }
+        public static char[] getDiagonal1(int diagonal, char[,] board) //diagonal ceetween 0 and Max(GetLength(0),GetLength(1))
+        {
+            return(Enumerable.Range(0, diagonal+2).Select(x => board[x,diagonal+1-x]).ToArray());
+        }
+        public static char[] getDiagonal2(int diagonal, char[,] board) //diagonal ceetween 0 and Max(GetLength(0),GetLength(1))
+        {
+            return(diagonal<0?Enumerable.Range(0, Math.Min(tab.GetLength(0)-Math.Abs(diagonal),tab.GetLength(1))).Select(x => tab[Math.Abs(diagonal)+x,x]).ToArray():Enumerable.Range(0, Math.Min(board.GetLength(0),board.GetLength(1)-diagonal)).Select(x => board[x,diagonal+x]).ToArray());
+        }
     }
 }
--- Dictionary.cs
+++ Dictionary.cs
@@ -22,6 +22,33 @@ namespace Word_Scramble
             File.WriteAllText(PathJSON, json);
             return data;
         }
+
+        //create a function that take a dictionnary and return a list of wordspublic static List<string> GetWords(Dictionary<int, string[]> data)
+        public static List<string> GetWords(Dictionary<int, string[]> data)
+        {
+            List<string[]> words = data.Values.ToList();
+            //transform words into a strig list
+            List<string> wordsList = new List<string>();
+            foreach (string[] word in words)
+            {
+                //add to wordsList word.toList()
+                wordsList.AddRange(word.ToList());
+            }
+            
+            return wordsList;
+        }
+
+        #endregion
+
+        #region Dictionnary methods
+        /// <summary> Redefine the dictionnary from the tab size </summary>
+        /// <param name="data"> Dictionnary of words </param>
+        /// <param name="tab"> Actual board </param>
+        /// <param name="size"> Size of the new dictionary </param>
+        public static Dictionary<int, string[]> redefineDictionary(Dictionary<int,string[]> data,char[,] tab,int size)
+        {
+            return data.Where(x => (x.Key<= size)).ToDictionary(x => x.Key, x => x.Value);
+        }
         #endregion
 
     }
