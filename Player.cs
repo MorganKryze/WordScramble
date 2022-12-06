066e06ec5e09bd3e69064680f3d6cd796b3b4bf1
Morgan <yann.vidamment@edu.devinci.fr>
2022-12-06 20:45:29 +0100

    Added Player class and PlayersList storage

--- Player.cs
+++ Player.cs
@@ -5,8 +5,46 @@ using System.Threading.Tasks;
 
 namespace Word_Scramble
 {
+    /// <summary>The player class.</summary>
     public class Player
     {
-        
+        #region Attributes 
+        /// <summary>The name of the player.</summary>
+        public string Name { get; set; }
+        /// <summary>Wether the player has finished the game or not.</summary>
+        public bool InGame { get; set; }
+
+        /// <summary>The score of the player.</summary>
+        public int Score { get; set; }
+        /// <summary>The maximum number of words found by the player.</summary>
+        public int MaxScore { get; set; }
+        /// <summary>The words found by the player.</summary>
+        public List<string>? Words { get; set; }
+        #endregion
+
+        #region Constructor
+        /// <summary>The constructor of the player class.</summary>
+        /// <param name="name">The name of the player.</param>
+        /// <param name="inGame">Whether the player's game has ended or not.</param>
+        /// <param name="score">The score of the player.</param>
+        /// <param name="maxScore">The maximum score of the player.</param>
+        /// <param name="words">The list of words found by the player.</param>
+        /// <returns>A new player.</returns>
+        public Player(string name = "", bool inGame = false, int score = 0, int maxScore = 0,List<string>? words = null)
+        {
+            Name = name;
+            InGame = inGame;
+            Score = score;
+            MaxScore = maxScore;
+            Words = words;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>This method is used to display every data about the player.</summary>
+        /// <returns>A string with every data.</returns>
+        public override string ToString() => $"Name: {Name}, InGame: {InGame}, Score: {Score}, MaxScore: {MaxScore}, Words: {Words}";
+
+        #endregion
     }
 }
