ebd6cf3c91b15639c1878324b4b17036a9f40517
Morgan <yann.vidamment@edu.devinci.fr>
2022-12-07 23:58:13 +0100

    Re-updated Dictionary class

--- Dictionary.cs
+++ Dictionary.cs
@@ -1,43 +1,62 @@
 using Newtonsoft.Json;
 
-namespace Word_Scramble
+namespace AUTRE
 {
-    public class Dictionary
-    { 
-        /// <summary> Dictionnary of the class Dictionary</summary>
-        //need a set in order to delete a value once placed in the grid
-        public Dictionary<int, List<string>> dictlist{ get; set; }
+    /// <summary>The dictionnary creation class.</summary>
+    class Dictionary
+    {
+        #region Attributes
+        public static string s_Language = "FR";
+        /// <summary>The dictionnary itself.</summary>
+        public static Dictionary<int, string[]> s_Dict = new Dictionary<int, string[]>();
+        public static Dictionary<int, List<string>> s_DictList = new Dictionary<int, List<string>>();
+        #endregion
+        
+        #region Constructor
+        /// <summary>The constructor of the class.</summary>
+        public Dictionary()
+        {
+            string[] lines = File.ReadAllLines("dataDictionary/MotsPossiblesFR.txt");
+            int[] Key = lines.Where((x, i) => i % 2 == 0).Select(x => int.Parse(x)).ToArray();
+            string[][] Value = lines.Where((x, i) => i % 2 != 0).Select(x => x.Split(' ')).ToArray();
 
-        /// <summary> Constructor of the class Dictionary</summary>
+            s_Dict = Key.Zip(Value, (s, i) => new { s, i }).ToDictionary(item => item.s, item => item.i);
+        }
         public Dictionary(Dictionary<int, string[]> dictionary)
         {
             
-            dictlist = dictionary.ToDictionary(x => x.Key, x => x.Value.ToList());
+            s_DictList = dictionary.ToDictionary(x => x.Key, x => x.Value.ToList());
         }
+        #endregion
 
-
-        /// <summary> Function to read a txt file and store it in a dictionnary</summary>
-        /// <param name="PathTXT">Path of the txt file</param>
-        /// <returns>Dictionary</returns>
-        public static Dictionary<int, string[]> ReadTxt(string PathTXT)
-        {
-            //read the json file and store it in a string
-            string[] lines = File.ReadAllLines(PathTXT);
-            //create arrays of odd elements in lines (keys) ==> convert string[] into int[]
-            int[] Key = lines.Where((x, i) => i % 2 == 0).Select(x => int.Parse(x)).ToArray();
-            //create arrays of even elements in lines (values) ==> convert string[] into string[][]
-            string[][] Value = lines.Where((x, i) => i % 2 != 0).Select(x => x.Split(' ')).ToArray();
-            //dictionnary creation by Zip method
-            Dictionary<int, string[]> data = Key.Zip(Value, (s, i) => new { s, i }).ToDictionary(item => item.s, item => item.i);
-            return data;
-        }
-
-
+        #region Methods
         /// <summary> Function to redefine the dictionary to the size of the Dictionnary</summary>
-        /// <param name="data">Dictionary to redefine</param>
-        public static Dictionary<int, string[]> redefineDictionary(Dictionary<int,string[]> data,char[,] tab,int size)
+        /// <param name="data">Dictionary to redefine</param name>
+        public static Dictionary<int, string[]> redefineDictionary(Dictionary<int,string[]> data,int size)
         {
             return data.Where(x => (x.Key<= size)).ToDictionary(x => x.Key, x => x.Value);
         }
+        /*
+        public static bool Recherche(string mot)
+        {
+            return RechercheDicho(mot,s_Dictionary[mot.Length],0,s_Dictionary[mot.Length].Length-1);
+        }
+        public static bool RechercheDicho(string mot, string[]liste, int départ, int arrivée)
+        {
+            int milieu = (int)arrivée/2;
+            if (mot.Equals(liste[milieu]))return true;
+            else if(mot.CompareTo(liste[milieu])>0)return RechercheDicho(mot,s_Dictionary[mot.Length],départ,milieu);
+            else return RechercheDicho(mot,s_Dictionary[mot.Length],milieu,arrivée);
+        }
+        public static bool RechercheDicho(string mot, string[]liste, int départ, int arrivée)
+        {
+            if(départ>arrivée)return false;
+            int milieu = (int)arrivée/2;
+            if (mot.Equals(liste[milieu]))return true;
+            else if(mot.CompareTo(liste[milieu])>0)return RechercheDicho(mot,s_Dictionary[mot.Length],départ,milieu);
+            else return RechercheDicho(mot,s_Dictionary[mot.Length],milieu,arrivée);
+        }*/
+        #endregion
+
     }
 }
