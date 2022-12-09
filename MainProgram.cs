eea868813714bbc4df6f6a01d954b4e77f31d054
Morgan <yann.vidamment@edu.devinci.fr>
2022-12-10 00:17:02 +0100

    Updated demo MainProgram

--- MainProgram.cs
+++ MainProgram.cs
@@ -12,27 +12,25 @@ namespace Word_Scramble
             Methods.ConsoleConfig();
             #endregion
 
-            //Main_Menu :
+            Main_Menu :
 
             #region Lobby
             Methods.MainMenu();
             #endregion
-
-            //lignes de test
-            Methods.SelectWords(Methods.CsvToMatrix("dataGrills/ComplexGrill.csv"), new List<string>{"TERRE","FERME","RN","RAI"});
-            //Methods.SelectWords(Methods.CsvToMatrix("dataGrills/ComplexGrill.csv"), new List<string>{"AGRICULTURE","BREBIS","CANARD","CHEVRES","CIDRE","CONFITURE","ELEVEUR","ESCARGOT","ESTIVE","FERME","FROMAGE"});
-
-            //Player_Creation_1 :
-
-            #region Setting up the player 1
-            #endregion
-
-            //Player_Creation_2 :
-
-            #region Setting up the player 2
-            #endregion
             
-            Methods.MainMenu();
+            //Players_Creation :
+
+            #region Setting up the players
+            Player player1 = new Player();
+            Player player2 = new Player();
+            if(!Methods.DefinePlayers(player1, player2))goto Main_Menu;
+            #endregion
+
+            #region Game loop
+            Methods.SelectWords(Methods.CsvToMatrix("dataGrills/ComplexGrill.csv"), new List<string>{"TERRE","FERME","ROCHE","ECHEC"});
+            #endregion
+
+            goto Main_Menu;
         }
     }
 }
